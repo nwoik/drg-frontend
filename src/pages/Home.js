6fb90fa9dfe348f7bb0fbbf1f057b9e3fafa41fb
nwoik <71194216+nwoik@users.noreply.github.com>
2023-10-10 17:16:47 +0100

    init

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React, { useRef, useState } from "react";
+import {Link} from "react-router-dom";
+import React, { useRef, useState } from "react";
+
+function Home() {
+    const urlRef = useRef()
+
+    function callit() {
+        
+    }
+
+    return (
+        <div>Deez nuts</div>
+    )
+
+}
+
+export default Home;
