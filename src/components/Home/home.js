0a8c8f44fc563846f6f71fc7080677bc1d129e68
nwoik <71194216+nwoik@users.noreply.github.com>
2023-10-20 11:49:56 +0100

    Made the dd and missions clickable

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -3,16 +3,16 @@ import './home.css'
 function Home() {
     return (
         <div className='main'>
-            <div id="page" className='missions'>
+            <a href="/deepdives" id="page" className='deepdives'>
                 <img src='DRG icon.png' alt=''></img>
                 <h1>Deep Dives</h1> 
                 <img src='DRG icon.png' alt=''></img>
-            </div>
-            <div id="page" className='deepdives'>
+            </a>
+            <a href="/missions" id="page" className='missions'>
                 <img src='DRG icon.png' alt=''></img>
                 <h1>Missions</h1>
                 <img src='DRG icon.png' alt=''></img>
-            </div>
+            </a>
         </div>
     )
 }
